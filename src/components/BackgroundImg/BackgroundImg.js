d171f954399a095e88cc8817947e3801e0e11de1
Misha <gorban77@gmail.com>
2020-02-24 13:22:52 +0200

    init BgImg

--- src/components/BackgroundImg/BackgroundImg.js
+++ src/components/BackgroundImg/BackgroundImg.js
@@ -1,1 +1,21 @@
 import './BackgroundImg.css';
+
+const baseUrl = 'https://pixabay.com/api/';
+const key = '&key=15364832-46e4bda7ae3c94390e1b1153f';
+
+    fetchImage() {
+        const requestParams = `?image_type=photo&orientation=horizontal&q=kiev&page=1&per_page=20`
+        return fetch(baseUrl + requestParams + key)
+            .then(response => response.json())
+            .then(parsedResponse => {
+                return parsedResponse.hits
+            });
+        };
+
+
+
+    BgChange = document.body.style,
+
+    fChange = function(){
+    BgChange.backgroundImage='url("")';
+    };
