78415e6364465ec99421ac0fcbfce56b16d3b8b2
romanitwork <workromanmail@gmail.com>
2020-02-24 14:04:48 +0200

    mobile v1

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,1 +1,22 @@
 import './Schedule.css';
+import canvas from './canvasjs.min.js';
+
+const scheduleButtons = document.querySelector('.schedule__div');
+const scheduleButtonText = document.querySelector('.show-charts');
+const ellipse = document.querySelector('.ellipse-img');
+const scheduleSection = document.querySelector('.schedule');
+
+scheduleButtons.addEventListener('click', openChart);
+
+function openChart(e){
+    ellipse.classList.toggle('ellipse-img-open');
+    scheduleSection.classList.toggle('none-schedule');
+    if(ellipse.classList.contains('ellipse-img-open')){
+        scheduleButtonText.textContent = 'Hide Chart'
+    }
+    else{
+        scheduleButtonText.textContent = 'Show Chart'
+    }
+
+}
+
