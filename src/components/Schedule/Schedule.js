688f6e4a1053f554b207a56d09f7536899dbab98
romanitwork <workromanmail@gmail.com>
2020-02-24 16:02:39 +0200

    schedule htmlcss done

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -2,21 +2,20 @@ import './Schedule.css';
 import canvas from './canvasjs.min.js';
 
 const scheduleButtons = document.querySelector('.schedule__div');
+const legend = document.querySelector('.schedule__legend');
 const scheduleButtonText = document.querySelector('.show-charts');
 const ellipse = document.querySelector('.ellipse-img');
 const scheduleSection = document.querySelector('.schedule');
 
 scheduleButtons.addEventListener('click', openChart);
 
-function openChart(e){
-    ellipse.classList.toggle('ellipse-img-open');
-    scheduleSection.classList.toggle('none-schedule');
-    if(ellipse.classList.contains('ellipse-img-open')){
-        scheduleButtonText.textContent = 'Hide Chart'
-    }
-    else{
-        scheduleButtonText.textContent = 'Show Chart'
-    }
-
+function openChart(e) {
+  ellipse.classList.toggle('ellipse-img-open');
+  scheduleSection.classList.toggle('none-schedule');
+  legend.classList.toggle('schedule__legend-none');
+  if (ellipse.classList.contains('ellipse-img-open')) {
+    scheduleButtonText.textContent = 'Hide Chart';
+  } else {
+    scheduleButtonText.textContent = 'Show Chart';
+  }
 }
-
