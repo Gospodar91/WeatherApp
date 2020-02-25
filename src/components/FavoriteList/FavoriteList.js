00bae61ba93a5c926836b1ec9bed10e0cee3ba75
unknown <arturbohanov1908@gmail.com>
2020-02-25 17:42:09 +0200

    add logic favorites ul

--- src/components/FavoriteList/FavoriteList.js
+++ src/components/FavoriteList/FavoriteList.js
@@ -1,15 +1,71 @@
 import './FavoriteList.css';
 
 import GlobalEmitter from '../GlobalFunctionAndVariables/EventEmitter';
-
 import favoritesList from './favoritesLi.hbs';
-const favorites = document.querySelector('.favorite-next');
+import PNotify from '../../../node_modules/pnotify/dist/es/PNotify.js';
+import PNotifyButtons from '../../../node_modules/pnotify/dist/es/PNotifyButtons.js';
+import '../../../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+
+const favorites = document.querySelector('.search__form-favourite');
 const input = document.querySelector('#search-input');
 const favoritesUl = document.querySelector('.favorites-list');
-const favoritesLi = document.querySelector('.favorites-list__item');
+const listBtn = document.querySelector('.favorite-next');
+
 favorites.addEventListener('click', onClickFavorites);
-function onClickFavorites(e) {
-  localStorage.setItem('town', 'input.value');
+let city;
+
+// function getDataFromLS() {
+//   const lsData = localStorage.getItem('town');
+//   if(lsData) {
+//     const parsedSettings = JSON.parse(lsData);
+//     city = parsedSettings;
+//     const markup = favoritesList({ city });
+//     favoritesUl.insertAdjacentHTML('beforeend', markup);
+//   }
+//   return [];
+// }
+function setDataInLS(city) {
+  const lsData = localStorage.getItem('town');
+  if (lsData) {
+    const parsedDataFromLs = JSON.parse(lsData);
+    localStorage.setItem('town', JSON.stringify([...parsedDataFromLs, city]));
+  } else {
+    localStorage.setItem('town', JSON.stringify([city]));
+  }
 }
 
+function onClickFavorites(e) {
+  city = input.value;
+  if (city.length >= 1) {
+    const markup = favoritesList({ city });
+    favoritesUl.insertAdjacentHTML('beforeend', markup);
+    setDataInLS(city);
+    // getDataFromLS()
+  } else {
+    PNotify.defaults.delay = 1200;
+    PNotify.error({
+      title: 'Oh No!',
+      text: 'Enter city!',
+    });
+  }
+}
 
+function savedStoreg() {
+  if (localStorage.getItem('town')) {
+    city = localStorage.getItem('town');
+    const markup = favoritesList({ city });
+    favoritesUl.insertAdjacentHTML('beforeend', markup);
+  }
+}
+savedStoreg();
+
+if (favoritesUl.children.length >= 1) {
+  const favoritesBtn = document.querySelector('.favorites-list__item-close');
+  const Ul = document.querySelector('.favorites-list__item');
+
+  favoritesBtn.addEventListener('click', btn);
+  function btn() {
+    Ul.remove();
+    localStorage.removeItem('town');
+  }
+}
