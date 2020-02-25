df39d78be56fb26f243a2790dfa3fe521a8621a0
grey4uk <grey4uk14@gmail.com>
2020-02-25 12:11:35 +0200

    move today weather on descktop more to right

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -1,6 +1,5 @@
 import './WeatherInfo.css';
 
-const weatherInfoTodayMain = document.querySelector('.weather-info');
 const weatherInfoToday = document.querySelector('.weather-today-wrapper');
 
 import GlobalEmitter from '../GlobalFunctionAndVariables/EventEmitter';
@@ -11,16 +10,15 @@ fiveDaysWeatherBtn.addEventListener('click', onFiveDaysWeatherBtnClick);
 
 function onFiveDaysWeatherBtnClick(e) {
   weatherInfoToday.classList.add('visually-hidden');
-  weatherInfoTodayMain.removeAttribute('right');
   fiveDaysWeatherBtn.classList.remove('weather-button-unactive');
   fiveDaysWeatherBtn.classList.add('weather-button-active');
   fiveDaysWeatherBtn.removeEventListener('click', onFiveDaysWeatherBtnClick);
   todayWeatherBtn.classList.add('weather-button-unactive');
   todayWeatherBtn.addEventListener('click', onTodayWeatherBtnClick);
-
 }
+
 function onTodayWeatherBtnClick(e) {
-    weatherInfoToday.classList.remove('visually-hidden');
+  weatherInfoToday.classList.remove('visually-hidden');
   todayWeatherBtn.classList.remove('weather-button-unactive');
   todayWeatherBtn.classList.add('weather-button-active');
   todayWeatherBtn.removeEventListener('click', onTodayWeatherBtnClick);
