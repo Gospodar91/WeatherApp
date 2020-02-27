cf61ae9237c6fda0311f4d216d3af3d65a018a01
Галина <galocka@MacBook-Galina.local>
2020-02-27 14:43:49 +0200

    already done, whithout hbs

--- src/components/MoreInfo/MoreInfo.js
+++ src/components/MoreInfo/MoreInfo.js
@@ -1,30 +1,42 @@
 import './MoreInfo.css';
 import GlobalEmitter from '../GlobalFunctionAndVariables/EventEmitter';
 import res from '../../services.js';
-let day = 0;
+const moment = require('moment');
+let day;
+// import hourlyWeatherTemp from './hourlyWeatherTemplate.hbs';
 
 const refs = {
-    fiveDaysList: document.querySelector('.FiveDaysWeaterList'),
-    hourlyWeather: document.querySelector('.hourly-weather'),
-}
-
-export default function showTemperature (res) {
-    refs.setTemperature.textContent = `${res.list[0].main.temp}`;
-    refs.setPressure.textContent = `${res.list[0].main['pressure']}mm`;
-    refs.setHumidity.textContent = `${res.list[0].main['humidity']}%`;
-    refs.setWind.textContent = `${res.list[0].wind['speed']}m/s`;
-    console.log('ooooooo',res.list[0].wind['speed'])
-  };
+  fiveDaysList: document.querySelector('.FiveDaysWeaterList'),
+  hourlyWeather: document.querySelector('.hourly-weather'),
+  fiveDaysItem: document.querySelector('.FiveDaysWeaterList__item'),
+  containerMoreInfo: document.querySelector('.MoreInfo'),
+};
 
 refs.fiveDaysList.addEventListener('click', handlerWeatherDay);
-// refs.moreInfoSecondDay.addEventListener('click', handlerWeatherForSecondDay);
-
 
 function handlerWeatherDay(event) {
-  const 
+  if (event.currentTarget === event.target) {
+    return;
+  }
+  refs.containerMoreInfo.style.display = 'block';
+  const dataAtribute = event.target.getAttribute('data-day');
+  day = dataAtribute;
+  const newArray = filterArray(res.fiveDay['list'], day);
+  console.log(newArray);
+
+//   const markupHourlyWeather = newArray.reduce(
+//     (acc, city) => acc + hourlyWeatherTemp(city),
+//     "",);
+
+    console.log('hghgjhgj', markupHourlyWeather)
+
+//   refs.hourlyWeather.insertAdjacentHTML('beforeend', markupHourlyWeather);
 }
 
-// в item поступит ссылка на лишку
-function showDataAtribute (item) {
-    const dataAtribute = item.getAtribute("");
+function filterArray(array, letDay) {
+  return array.filter(item => moment(item['dt'] * 1000).format('D') == letDay);
+}
+
+function clearHourlyWeatherContainer() {
+  refs.country.innerHTML = '';
 }
