73f99112763140dbfda28ddcf758c08f3f736a2a
grey4uk <grey4uk14@gmail.com>
2020-02-27 15:18:39 +0200

    fix notify

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -25,8 +25,11 @@ function submitForm(event) {
   /////////виправляв Стахура///////////////////////
   if (choiseInput.value !== services.city) {
     services.city = choiseInput.value;
-    services.getTodayWeather(services.city);
-    services.getFiveDayWeather(services.city);
+    if (services.blockSection === 'today') {
+        services.getTodayWeather(services.city);
+      } else {
+        services.getFiveDayWeather(services.city);
+      }
     services.getImgBackground(services.city);
   }
   // if (services.blockSection === 'today') {
