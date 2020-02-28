5974afd562ae535bbb4d8f8736197a8945ebf965
Misha <gorban77@gmail.com>
2020-02-28 10:26:59 +0200

    BgImg

--- src/components/BackgroundImg/BackgroundImg.js
+++ src/components/BackgroundImg/BackgroundImg.js
@@ -1,18 +1,24 @@
 import css from'./BackgroundImg.css';
+import spinner from '../Loader/loader.css';
+import spinner from '../Loader/loader.js';
+
+
 
 const baseUrl = 'https://pixabay.com/api/';
 const key = '&key=15364832-46e4bda7ae3c94390e1b1153f';
 const requestParams = `?image_type=photo&orientation=horizontal&q=kiev&page=1&per_page=20`
 const mainDiv = document.querySelector(".background-image")
 
-
+spinner.show();
 const fetchImage = () => {
     const baseUrl = 'https://pixabay.com/api/';
     const key = '&key=15364832-46e4bda7ae3c94390e1b1153f';
     const requestParams = `?image_type=photo&orientation=horizontal&q=kiev&page=1&per_page=40`
     
     return fetch(baseUrl + requestParams + key)
-        .then(response => response.json())
+        .then(response => {
+            spinner.hide();
+            response.json()})
         .then(parsedResponse => {
            
         const imgArr = parsedResponse.hits;
