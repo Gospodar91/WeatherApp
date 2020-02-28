bb660304841cf032ead0fe77662abfbe18bc0a74
Serbin9 <serbin_y@ukr.net>
2020-02-28 14:11:29 +0200

    final

--- src/components/CubeAnimation/CubeAnimation.js
+++ src/components/CubeAnimation/CubeAnimation.js
@@ -1,28 +1,29 @@
 import './CubeAnimation.css';
 import gsap from 'gsap';
-document.addEventListener('DOMContentLoaded', function (e) {
+
+document.addEventListener('DOMContentLoaded', qubeAnimation);
+
+function qubeAnimation() {
     let scene = document.querySelector(".gw-parts-scene");
     if (!!!scene) {
         return;
     }
-    for (let i = 0; i < 3; i++) {
-        GWcreatePart(scene);
+    for (let i = 0; i < 2; i++) {
+        qwcreatePart(scene);
     }
-    function GWcreatePart(scene) {
+    function qwcreatePart(scene) {
         let part = document.createElement('div');
-        part.className = "gw-part";
-        part.innerHTML = "WHY NOT?";
+        part.classList.add("gw-part");
         let partItem = document.createElement('div');
-        partItem.className = "gw-part__item";
-        partItem.innerHTML = "TIME TO TRAVEL";
-       scene.appendChild(partItem);
+        partItem.classList.add("gw-part__item");
+        // let partItemLeft = documnet.createElement('div');
+        // partItemLeft.classList.add('left');
+        scene.appendChild(partItem);
         scene.appendChild(part);
-      
+        // scene.appendChild(partItemLeft);
     }
-    const sX =  100-Math.random()*200;
-    const sY =  100-Math.random()*200;
-    const sx_item = 250-Math.random()*400;
-    const sy_item = 250-Math.random()*400;
-    gsap.to('.gw-part', {duration:30, x:sX, y:sY, borderRadius:'15px', rotate:360, backgroundColor:'transparent', border: "3px solid #d5d7d8"});
-    gsap.to('.gw-part__item', {duration:30, borderRadius:'15px', x:sx_item, y:sy_item, rotate:360, backgroundColor:'transparent', border: "3px solid #d5d7d8", skale: 2});
-});
+    gsap.to('.gw-part', {duration:30, x:-500, y:-420, borderRadius:'15px', rotate:500, backgroundColor:'transparent', border: "2px solid white",scale:1.2});
+    gsap.to('.gw-part__item', {duration:30, borderRadius:'15px', x:500, y:-400, rotate:340, backgroundColor:'transparent', border: "2px solid white", scale:1.2});
+    // gsap.to('left', {duration:30, borderRadius:'15px', x:500, y:400, rotate:340, backgroundColor:'transparent', border: "2px solid white", scale:1.2})
+};
+
