bb953a2ead02940ea6b9d9afcffca2793d5995d2
Sergei <xsergeix@ukr.net>
2020-03-01 21:48:32 +0300

    pre More Info Merge

--- src/components/BackgroundImg/BackgroundImg.js
+++ src/components/BackgroundImg/BackgroundImg.js
@@ -9,7 +9,7 @@ const mainDiv = document.querySelector(".background-image")
 const fetchImage = () => {
     const baseUrl = 'https://pixabay.com/api/';
     const key = '&key=15364832-46e4bda7ae3c94390e1b1153f';
-    const requestParams = `?image_type=photo&category=travel&orientation=horizontal&q=kiev&page=1&per_page=40`
+    const requestParams = `?image_type=photo&category=places&orientation=horizontal&q=kiev&page=1&per_page=40`
     
     return fetch(baseUrl + requestParams + key)
         .then(response => {
