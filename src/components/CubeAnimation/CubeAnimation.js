ef887877f38f26bd6d6cb6663e5db3c3475a6dd0
Sergei <xsergeix@ukr.net>
2020-03-02 12:26:07 +0300

    stable cubeAnimation commit

--- src/components/CubeAnimation/CubeAnimation.js
+++ src/components/CubeAnimation/CubeAnimation.js
@@ -1,24 +1,53 @@
 import './CubeAnimation.css';
+
 import gsap from 'gsap';
 
 document.addEventListener('DOMContentLoaded', qubeAnimation);
 
 function qubeAnimation() {
-    let scene = document.querySelector(".gw-parts-scene");
-    if (!!!scene) {
-        return;
-    }
-    for (let i = 0; i < 2; i++) {
-        qwcreatePart(scene);
-    }
-    function qwcreatePart(scene) {
-        let part = document.createElement('div');
-        part.classList.add("gw-part");
-        let partItem = document.createElement('div');
-        partItem.classList.add("gw-part__item");
-        scene.appendChild(partItem);
-        scene.appendChild(part);
-    }
-    gsap.to('.gw-part', {duration:30, x:-500, y:-420, borderRadius:'15px', rotate:500, backgroundColor:'transparent', border: "5px solid white",scale:1.2});
-    gsap.to('.gw-part__item', {duration:30, borderRadius:'15px', x:500, y:-400, rotate:340, backgroundColor:'transparent', border: "5px solid white", scale:1.2});
-};
+  let scene = document.querySelector('.gw-parts-scene');
+
+  if (!!!scene) {
+    return;
+  }
+
+  for (let i = 0; i < 2; i++) {
+    qwcreatePart(scene);
+  }
+
+  function qwcreatePart(scene) {
+    let part = document.createElement('div');
+
+    part.classList.add('gw-part');
+
+    let partItem = document.createElement('div');
+
+    partItem.classList.add('gw-part__item');
+
+    scene.appendChild(partItem);
+
+    scene.appendChild(part);
+  }
+
+  gsap.to('.gw-part', {
+    duration: 30,
+    x: -500,
+    y: -440,
+    borderRadius: '15px',
+    rotate: 500,
+    backgroundColor: 'transparent',
+    border: '5px solid rgba(255, 255, 255, 0.6)',
+    scale: 1.2,
+  });
+
+  gsap.to('.gw-part__item', {
+    duration: 30,
+    borderRadius: '15px',
+    x: 460,
+    y: -410,
+    rotate: 340,
+    backgroundColor: 'transparent',
+    border: '5px solid rgba(255, 255, 255, 0.6)',
+    scale: 1.2,
+  });
+}
