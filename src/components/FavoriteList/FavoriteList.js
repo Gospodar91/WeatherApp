455196e4d1e975ce85ac8261a13227d3b4b45ba1
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-03-02 19:08:25 +0200

    fix

--- src/components/FavoriteList/FavoriteList.js
+++ src/components/FavoriteList/FavoriteList.js
@@ -5,6 +5,8 @@ import favoritesLocal from './favoritesLocal.hbs';
 import PNotify from '../../../node_modules/pnotify/dist/es/PNotify.js';
 import PNotifyButtons from '../../../node_modules/pnotify/dist/es/PNotifyButtons.js';
 import '../../../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+import Loader from '../Loader/loader';
+
 
 const favorites = document.querySelector('.search__form-favourite');
 const input = document.querySelector('#search-input');
@@ -12,37 +14,35 @@ const favoritesUl = document.querySelector('.favorites-list');
 const nextButton = document.querySelector('.favorite-next');
 const prevButton = document.querySelector('.favourite-prev');
 
+
 nextButton.hidden = true;
 prevButton.hidden = true;
 
 let qtyClickBtn = 0;
 let choiseLii = favoritesUl.children;
-let lenghtLiChild = favoritesUl.children.length;
 let clientWidth = document.documentElement.clientWidth;
 
-//function for drawing and deleting buttons
 function checkQtyLi() {
+  let key;
   if (favoritesUl.children.length) {
-    let key = JSON.parse(localStorage.getItem('town'));
+     key = JSON.parse(localStorage.getItem('town'));
     if (clientWidth < 771) {
       if (key.length > 2) {
         nextButton.hidden = false;
       }
     } else if (clientWidth > 771) {
-      if (key.length > 4) {
-        nextButton.hidden = false;
-      }
+        nextButton.hidden = !(key.length > 4);
+        prevButton.hidden = !(qtyClickBtn>0);
     }
   }
 }
 
-//left button
+
 prevButton.addEventListener('click', onClickPrevBtn);
 function onClickPrevBtn(event) {
+  let lenghtLiChild = favoritesUl.children.length;
   qtyClickBtn--;
-  if (qtyClickBtn < lenghtLiChild + 1) {
-    prevButton.hidden = true;
-  }
+    prevButton.hidden = (qtyClickBtn < 1);
   nextButton.hidden = false;
   choiseLii.forEach(li => {
     li.style.transform += 'translateX(113px)';
@@ -50,7 +50,6 @@ function onClickPrevBtn(event) {
   });
 }
 
-//right button
 nextButton.addEventListener('click', onClickNextBtn);
 function onClickNextBtn(event) {
   let clientWidth = document.documentElement.clientWidth;
@@ -66,6 +65,7 @@ function onClickNextBtn(event) {
     li.style.transform += 'translateX(-113px)';
     li.style.transitionDuration = 500 + 'ms';
   });
+  receiveLenghtLi();
 }
 
 let city = input;
@@ -75,7 +75,6 @@ export function onClickFavorites() {
     favoritesUl.innerHTML = '';
     setDataInLS(city);
     getDataFromLS();
-    favorites.classList.add('bgNew');
     checkQtyLi();
   } else {
     PNotify.defaults.delay = 1200;
@@ -97,36 +96,76 @@ function setDataInLS(city) {
       });
       return;
     }
-    localStorage.setItem('town', JSON.stringify([...parsedDataFromLs, city]));
+    verificationCorectCitybyWeatherApi(city, parsedDataFromLs);
   } else {
-    localStorage.setItem('town', JSON.stringify([city]));
+    verificationCorectCitybyWeatherApi(city);
+    document
+      .querySelector('.search__form-favourite')
+      .removeEventListener('click', onClickFavorites);
   }
 }
 function getDataFromLS() {
+  favoritesUl.innerHTML ='';
   const lsData = localStorage.getItem('town');
   if (lsData) {
     const parsedSettings = JSON.parse(lsData);
     const markup = favoritesLocal({ parsedSettings });
     favoritesUl.insertAdjacentHTML('beforeend', markup);
+    checkQtyLi();
   }
-  checkQtyLi();
 }
 getDataFromLS();
-if (favoritesUl.children.length) {
-  favoritesUl.addEventListener('click', onClickLink);
-  function onClickLink(e) {
-    e.preventDefault();
-    if (e.target === e.currentTarget) {
-      return;
-    } else if (e.target.tagName === 'BUTTON') {
-      const lsData = JSON.parse(localStorage.getItem('town'));
-      const lsDataFilter = lsData.filter(el => el !== e.target.dataset.text);
-      localStorage.setItem('town', JSON.stringify(lsDataFilter));
-      e.target.parentNode.remove();
-    } else {
-      services.city = input.value = e.target.textContent;
-      GlobalEmitter.emit(GlobalEmitter.ON_SEND_SUBMIT_FROM_FAVORITES, e);
-      favorites.classList.add('bgNew');
-    }
+favoritesUl.addEventListener('click', onClickLink);
+function onClickLink(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  } else if (e.target.tagName === 'BUTTON') {
+    const lsData = JSON.parse(localStorage.getItem('town'));
+    const lsDataFilter = lsData.filter(el => el !== e.target.dataset.text);
+    localStorage.setItem('town', JSON.stringify(lsDataFilter));
+    e.target.parentNode.remove();
+    qtyClickBtn--;
+    getDataFromLS();
+  } else {
+    //поиск по городу local storage
+    services.city = input.value = e.target.textContent;
+    //GlobalEmitter.emit(GlobalEmitter.ON_SEND_SUBMIT_FROM_FAVORITES, e);
+    searchWeatherAndBackgroungOnCityFromLs(e.target.textContent);
+    favorites.classList.add('bgNew');
   }
+}
+function searchWeatherAndBackgroungOnCityFromLs(city){
+  if(services.blockSection === 'today') {
+    services.getTodayWeather(city);
+  } else if (services.blockSection === 'fiveDay'){
+    services.getFiveDayWeather(city);
+  }
+  services.getImgBackground(city);
+}
+function verificationCorectCitybyWeatherApi(city, parsedDataFromLs){
+  // Loader.show();
+  const baseUrlForTodayWeather =
+  'https://api.openweathermap.org/data/2.5/weather?APPID=8defc985a5e2c764076c53bf90c6c44e&units=metric&lang=en&q=';
+  fetch(baseUrlForTodayWeather + city)
+  .then(res => {
+    if (res.status === 404) {
+      PNotify.error({
+        title: 'NOTICE!',
+        text: "Can't add such city!",
+      });
+    }
+    if (res.status !== 404){
+      if(parsedDataFromLs) {
+        localStorage.setItem('town', JSON.stringify([...parsedDataFromLs, city]));
+        getDataFromLS();
+      } else {
+        localStorage.setItem('town', JSON.stringify([city]));
+      }
+    }
+  })
+  .catch(err => {
+    console.error(error);
+  })
+  // .finally(() => setTimeout(Loader.hide, 500))
 }
